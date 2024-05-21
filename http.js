c9b8d4c09a75b538bcc8a6f320b1f40c4aec0fe6
Sardorbek <studentofinha@gmail.com>
2024-05-21 18:20:19 +0500

    added

---
+++ http.js
@@ -0,0 +1,12 @@
+const {request}=require('https')
+
+const req=request('https://www.google.com',(res)=>{
+    res.on('data',(chunk)=>{
+        console.log(chunk)
+    })
+    res.on('end',()=>{
+
+    })
+})
+
+req.end()
